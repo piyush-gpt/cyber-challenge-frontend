1014bef10babf3cc212e6d0f22a68996ef7a304e
piyushgpt <piyushgpt10102004@gmail.com>
2025-03-16 18:38:22 +0530

    made api naming changes to deploy

--- src/services/apis.tsx
+++ src/services/apis.tsx
@@ -1,8 +1,8 @@
-const BASE_URL = import.meta.env.REACT_APP_BASE_URL ?? "http://localhost:5000/api";
+const BASE_URL = import.meta.env.REACT_APP_BASE_URL ?? "http://localhost:5000";
 
-export const TEXT_DETECTION_URL = BASE_URL + "/text/analyze-text";
-export const AUDIO_DETECTION_URL = BASE_URL + "/audio/analyze-audio";
-export const VIDEO_DETECTION_URL = BASE_URL + "/video/analyze-video";
+export const TEXT_DETECTION_URL = BASE_URL + "/api/text/analyze-text";
+export const AUDIO_DETECTION_URL = BASE_URL + "/api/audio/analyze-audio";
+export const VIDEO_DETECTION_URL = BASE_URL + "/api/video/analyze-video";
 
 
-export const REPORT_SCAM_URL = BASE_URL + '/report-scam';
+export const REPORT_SCAM_URL = BASE_URL + '/api/report-scam';
